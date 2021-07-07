eb9531bc05b5616af176eaf820bf6b076233b497
AlanPL <apyolt@gmail.com>
2021-07-07 18:22:22 -0500

    edit profile and change password

--- backend/app/controllers/user.js
+++ backend/app/controllers/user.js
@@ -1,41 +1,76 @@
-const mongoose = require('mongoose');
-const User = require('../models/user');
+const mongoose = require("mongoose");
+const User = require("../models/user");
 module.exports = {
-    addUser:async (data) =>{
-        try{   
-            data.type=1;
-            let user = await new User(data).save();
-            return user;
-        }catch(error){
-            console.log(error);
-            return -2;
-        }
-    },
-    getUserById:async function(id){
-        try{
-            const user = await User.findOne({_id: new mongoose.Types.ObjectId(id)}, 'createdRecipes _id username email type');
-            if(!user) return -1;
-            else return user;
-        }catch(error){
-            console.log(error);
-            return -2;
-        }
-    },
-    getUserByEmailAndPassword:async function(email, password){
-        try{
-            const userId = await User.findOne({email, password}, '_id');
-            console.log(userId);
-            if(!userId) return -1;
-            else{
-                const user = await this.getUserById(userId._id);
-                if(user === -1 || user === -2)
-                    return -2;
-                else
-                    return user;
-            }
-        }catch(error){
-            console.log(error);
-            return -3;
-        }
-    },
+  
+  getUserById: async function (id) {
+    try {
+      const user = await User.findOne(
+        { _id: new mongoose.Types.ObjectId(id) },
+        "createdRecipes _id username email type"
+      );
+      if (!user) return -1;
+      else return user;
+    } catch (error) {
+      console.log(error);
+      return -2;
+    }
+  },
+  getUserByEmailAndPassword: async function (email, password) {
+    try {
+      const userId = await User.findOne({ email, password }, "_id");
+        console.log("userId:"+userId);
+      if (!userId) return -1;
+      else {
+        const user = await this.getUserById(userId._id);
+        if (user === -1 || user === -2) return -2;
+        else return user;
+      }
+    } catch (error) {
+      console.log(error);
+      return -3;
+    }
+  },
+  addUser: async (data) => {
+    try {
+      data.type = 1;
+      let user = await new User(data).save();
+      return user;
+    } catch (error) {
+      console.log(error);
+      return -2;
+    }
+  },
+  updateUser: async (id, data) => {
+    try {
+      const user = await User.findByIdAndUpdate(id, {
+        $set: {
+          username: data.username,
+          email: data.email,
+          picture: data.picture,
+        },
+      }, {new:true, select: "createdRecipes _id username email type"});
+      if (!user) return -1;
+      else return user;
+    } catch (error) {
+      console.log(error);
+      return -2;
+    }
+  },
+  updateUserPassword: async (id, data) => {
+    try {
+      var user = await module.exports.getUserByEmailAndPassword(data.email, data.password);
+      if (user=== -1) return -1;
+      else {
+        user = await User.findByIdAndUpdate(id, {
+          $set: {
+            password: data.newPassword
+          },
+        }, {new:true, select: "createdRecipes _id username email type"});
+        return user;
+      }
+    } catch (error) {
+      console.log(error);
+      return -2;
+    }
+  }
 };
--- backend/app/routes/user.js
+++ backend/app/routes/user.js
@@ -17,6 +17,44 @@ router.post('/', async (req, resp, next) => {
     }   
 });
 
+//edit general user info
+router.patch('/:id', async (req, resp, next) => {
+    if( req.session.user?._id===req.params.id || req.session.user?.type===3){
+        if(req.body != null ){
+            const response = await UserController.updateUser(req.params.id, req.body);
+            if(response === -3 || response === -2)
+                return resp.status(500).json({status: "error", description: "Database connection error"});
+            else if(response === -1)
+                return resp.status(401).json({status: "error", description:"User not found"});
+            else
+                return resp.header('Access-Control-Allow-Origin', '*').status(200).send({status: "ok", data: response});
+        }else{
+            return resp.status(400).json({status:"error", description:"Wrong parameters"});
+        }
+    }else{
+        return resp.status(401).json({status:"error", description:"You must be logged and have the proper permissions"});
+    }
+});
+
+//change user password
+router.patch('/password/:id', async (req, resp, next) => {
+    if( req.session.user?._id===req.params.id ){
+        if(req.body?.newPassword === req.body?.confirmPassword){
+            const response = await UserController.updateUserPassword(req.params.id, req.body);
+            if(response === -3 || response === -2)
+                return resp.status(500).json({status: "error", description: "Database connection error"});
+            else if(response === -1)
+                return resp.status(401).json({status: "error", description:"User not found"});
+            else
+                return resp.header('Access-Control-Allow-Origin', '*').status(200).send({status: "ok", data: response});
+        }else{
+            return resp.status(400).json({status:"error", description:"Wrong parameters"});
+        }
+    }else{
+        return resp.status(401).json({status:"error", description:"You must be logged and have the proper permissions"});
+    }
+});
+
 //get user info
 router.get('/:id', async (req, resp, next) => {
     if(req.params == null ){
